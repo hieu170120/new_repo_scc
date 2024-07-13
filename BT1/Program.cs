7cfecd2768bb7a023f24e4e641f11391e18eacf6
hieu170120 <161483292+hieu170120@users.noreply.github.com>
2024-07-13 23:46:08 +0700

    Add project files.

---
+++ BT1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.LoiChao();
+            Student student = new Student();
+            student.HocBai();
+            student.SetAge(20);
+            student.Thongbaotuoi();
+            Teacher teacher = new Teacher();
+            teacher.Giangday();
+
+
+        }
+    }
+}
+
+public class Person
+{
+    protected int age;
+
+    public Person()
+    {
+    }
+
+    public void LoiChao()
+    {
+        Console.WriteLine("Xin chao");
+    }
+
+    public void SetAge(int n)
+    {
+        age = n;
+    }
+}
+public class Teacher : Person
+{
+    public Teacher()
+    {
+    }
+    public void Giangday()
+    {
+        Console.WriteLine("Oke vo tiet hoc");
+    }
+}
+public class Student : Person
+{
+    public Student()
+    {
+    }
+    public void HocBai()
+    {
+        Console.WriteLine("Dang hoc bai");
+    }
+    public void Thongbaotuoi()
+    {
+        Console.WriteLine("Tuoi cua hoc sinh la: " + age);
+    }
+}   
---
+++ Class2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Person
+{
+	public Person()
+	{
+		protected int age;
+	    public void LoiChao()
+	    {
+		 Console.WriteLine("Xin chao");	
+	    }
+	}
+    public void SetAge(int n)
+{
+	   age = n;
+}
+
+}
